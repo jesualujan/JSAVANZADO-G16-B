bc9ab674424c063265d02a65597254aa642636e5
Jesua Luján <code.masterqro@gmail.com>
2022-07-18 20:12:35 -0500

    creamos nuestro autor pasandole nuestro mini formato json

--- 05APIcrud/main.js
+++ 05APIcrud/main.js
@@ -5,4 +5,16 @@ const goodReadsCrud = require ('./crudAuthors.js')
 // goodReadsCrud.listAuthors()
 
 //* LISTA UN AUTOR POR SU ID
-goodReadsCrud.getAuthor(14573)
+ goodReadsCrud.getAuthor(14786) //14573  //14773 //14781
+
+//* CREAR UN AUTOR 
+// const jsonSend = {  
+//         name: "LEO",
+//         last_name: "MESSI",
+//         nacionalidad: "MX",
+//         biography: "EX FUTBOLISTA DEL BARCELONA",
+//         gender: "M",
+//         age: 33,
+//         is_alive: true
+//     }
+// goodReadsCrud.createAuthor(jsonSend)
