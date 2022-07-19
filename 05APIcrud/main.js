5b07673509a5c1ad2284280d94d1255ba57dc03a
Jesua Luján <code.masterqro@gmail.com>
2022-07-18 19:37:45 -0500

    traemos los bloques de código de crudAuthors y los ejecutamos

---
+++ 05APIcrud/main.js
@@ -0,0 +1,8 @@
+//* NUESTRO JS PRINCIPAL 
+const goodReadsCrud = require ('./crudAuthors.js')
+
+//* LISTA DE TODOS LOS AUTORES
+// goodReadsCrud.listAuthors()
+
+//* LISTA UN AUTOR POR SU ID
+goodReadsCrud.getAuthor(14573)
