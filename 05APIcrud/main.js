d020fc485f22a7fe4a038d366d1bd4b4053feaec
Jesua Luján <code.masterqro@gmail.com>
2022-07-18 20:53:40 -0500

    ejecutamos partial update y al final delete para tener nustro crud

--- 05APIcrud/main.js
+++ 05APIcrud/main.js
@@ -5,7 +5,7 @@ const goodReadsCrud = require ('./crudAuthors.js')
 // goodReadsCrud.listAuthors()
 
 //* LISTA UN AUTOR POR SU ID
- goodReadsCrud.getAuthor(14786) //14573  //14773 //14781
+//  goodReadsCrud.getAuthor(14786) //14573  //14773     //14781 - registro eliminado
 
 //* CREAR UN AUTOR 
 // const jsonSend = {  
@@ -18,3 +18,14 @@ const goodReadsCrud = require ('./crudAuthors.js')
 //         is_alive: true
 //     }
 // goodReadsCrud.createAuthor(jsonSend)
+
+//*PATCH A UN AUTOR
+//  const updateData = {
+//         name: "MARIA",
+//         last_name: "FERNANDA",
+//         biography: "Estudiante de arquitectura"
+//  }
+//  goodReadsCrud.patchAuthor(14786,updateData)
+
+//*ELIMINAR UN AUTOR 
+goodReadsCrud.deleteAuthor(14786)
